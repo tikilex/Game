9dc9ca1b0af8fada4f602220c87a81d5aa26cd5e
Rabogi <xkrules2002@mail.ru>
2021-10-03 22:50:16 +0300

    asa

--- Assets/MainMenu/MainMenu.cs
+++ Assets/MainMenu/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public GameObject completed;
+    public GameObject backgroundComp;
     public static void PlayGame(int Level)
     {   
         GlobalValues.Reset();
@@ -14,8 +15,10 @@ public class MainMenu : MonoBehaviour
 
     void Start(){
         SaveManager.InitialSave();
-        if(PlayerPrefs.GetInt("GameCompleted",0)==1)
+        if(PlayerPrefs.GetInt("GameCompleted",0)==1){
             completed.SetActive(true);
+            backgroundComp.SetActive(true);
+        }
     }
 
     public void ExitGame()
